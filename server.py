b54e934a98ca2b7db762be6b38cd535c2f0386e1
Alex Fitzwilliam <captzandra@gmail.com>
2024-01-04 21:11:42 -0700

    setting up react

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for movie ratings app."""
 
 from flask import (Flask, render_template, request, flash, session,
-                   redirect)
+                   redirect, jsonify)
 from model import db ,connect_to_db
 #import crud
 from jinja2 import StrictUndefined
