1d5ffc99dbe35f5c3a4cbb53fe0db329ebfba774
Alex Fitzwilliam <captzandra@gmail.com>
2024-01-20 13:56:25 -0700

    collected username and password input on create_account

--- FrontEnd/src/components/create_account/create_account.jsx
+++ FrontEnd/src/components/create_account/create_account.jsx
@@ -4,23 +4,38 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const CreateAccount = () => {
+    const [newUsername, setNewUsername] = React.useState("");
+    const [newPassword, setNewPassword] = React.useState("");
+
+
+    function handleSubmit(e) {
+
+        e.preventDefault();
+        const newUser={newUsername, newPassword}
+    
+        console.log(newUser)
+    }
+
+
 
     return (
         <div>
             <h1>Create Account</h1>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formUsername">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="username" placeholder="Enter username" />
+                    <Form.Control type="username" placeholder="Enter username" 
+                    value={newUsername} onChange={(e)=>setNewUsername(e.target.value)}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" 
+                    value={newPassword} onChange={(e)=>setNewPassword(e.target.value)}/>
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                {/* <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Retype Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                </Form.Group> */}
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
