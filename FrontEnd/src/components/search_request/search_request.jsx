9b6c6bdf28dc079aa912ac7b1059b34a728034ca
Alex Fitzwilliam <captzandra@gmail.com>
2024-02-06 21:45:48 -0700

    correctly passing in search type on frontend to handlesubmit

--- FrontEnd/src/components/search_request/search_request.jsx
+++ FrontEnd/src/components/search_request/search_request.jsx
@@ -6,11 +6,13 @@ import Form from 'react-bootstrap/Form';
 const SearchRequest = (props) => {
 
     const [searchParam, setSearchParam] = React.useState('');
+    const [searchType, setSearchType] = React.useState('')
 
     function handleSubmit(e) {
         e.preventDefault();
 
         console.log(`searchParam=${searchParam}`)
+        console.log(`searchType=${searchType}`)
 
         // const newShelf = {user_id, shelfName}
         // console.log(shelfName)
@@ -37,7 +39,18 @@ const SearchRequest = (props) => {
                 <Form.Group className="mb-3" controlId="formSearchRequest">
                     {/* <Form.Label>What would you like to name your shelf?</Form.Label> */}
                     <Form.Control placeholder="Type search keywords here" 
-                    value={searchParam} onChange={(e)=>setSearchParam(e.target.value)}/>
+                        value={searchParam} onChange={(e)=>setSearchParam(e.target.value)}/>
+                    <Form.Select 
+                        aria-label="How would you like to search?"
+                        value={searchType}
+                        onChange={e=>setSearchType(e.target.value)}
+                        // onChange={e=>console.log(e.target.value)}
+                        >
+                            <option>What would you like to search by?</option>
+                            <option value='title'>title</option>
+                            <option value='author'>author</option>
+
+                        </Form.Select>
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
