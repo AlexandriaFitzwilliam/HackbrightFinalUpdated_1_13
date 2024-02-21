12ebf2d2db59370db30779133f4473d71b54149c
Alex Fitzwilliam <captzandra@gmail.com>
2024-02-20 23:24:57 -0700

    rounding avg rating to 2 dec

--- BackEnd/model.py
+++ BackEnd/model.py
@@ -317,6 +317,8 @@ class Rating(db.Model):
 
         avg_rating = total_score / total_num
 
+        avg_rating = round(avg_rating, 2)
+
         book_user.avg_rating = avg_rating
         book_user.num_rating = total_num
 
